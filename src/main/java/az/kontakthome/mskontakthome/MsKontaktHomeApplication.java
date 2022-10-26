37dd39b55bd7fdcc0172478ff72954845b1f6bef
emiw8888 <cahangirliemil@gmail.com>
2022-10-26 14:48:42 +0400

    Initial commit

---
+++ src/main/java/az/kontakthome/mskontakthome/MsKontaktHomeApplication.java
@@ -0,0 +1,13 @@
+package az.kontakthome.mskontakthome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsKontaktHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsKontaktHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/kontakthome/mskontakthome/MsKontaktHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package az.kontakthome.mskontakthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsKontaktHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
